6b4dbb0ce827009ae7b24b4df14f8daab302dc4e
Pokerx95 <123877073+Pokerx95@users.noreply.github.com>
2024-10-08 12:45:26 +0800

    Customer Behaviour

--- Assets/Scripts/Customer/CustomerController.cs
+++ Assets/Scripts/Customer/CustomerController.cs
@@ -8,7 +8,6 @@ using UnityEngine;
 
 public class CustomerController : MonoBehaviour
 {
-    
     [SerializeField] private GameObject customerEntityPrefab;
     [SerializeField] private Transform customerSpawnPos;
     [SerializeField] private Transform[] customerPlacementPos;
@@ -18,6 +17,8 @@ public class CustomerController : MonoBehaviour
     private List<CustomerEntity> customerEntities;
     //counters to spawn next entity
     private float spawnEntityCounter;
+
+    private bool storeClosed;
 
     // Start is called before the first frame update
     void Start()
@@ -31,6 +32,8 @@ public class CustomerController : MonoBehaviour
         }
 
         spawnEntityCounter = 5f;
+
+        storeClosed = false;
     }
 
     // Update is called once per frame
@@ -46,18 +49,32 @@ public class CustomerController : MonoBehaviour
             //and spawn timer is zero
             if (customerEntities[i] == null)
             {
-                if (spawnEntityCounter <= 0)
+                if (spawnEntityCounter <= 0 && !storeClosed)
                 {
-                    Debug.Log("Spawn");
+                    //Debug.Log("Spawn");
                     //spawn entity
                     CustomerEntity newCustomer = Instantiate(customerEntityPrefab, customerSpawnPos).GetComponent<CustomerEntity>();
 
+                    //Assign food
+                    List<Dish.DishType> dishTypes = new List<Dish.DishType>();
+
+                    //TODO: Check if have enough ingredients for that food
+                    //Random between 1 or 2 foods
+                    int maxFoodNo = Random.Range(1, 3);
+                    for (int foodNo = 0; foodNo < maxFoodNo; foodNo++)
+                    {
+                        //Random Food types
+                        dishTypes.Add((Dish.DishType)Random.Range(0, (int)Dish.DishType.TotalDishes));
+                    }
+
                     //Give customer new placement position
                     newCustomer.Init(customerPlacementPos[i].position, customerEndPos.position);
+
+
                     //add customer entity to list
                     customerEntities[i] = newCustomer;
                     //reset timer
-                    spawnEntityCounter = 5f;
+                    spawnEntityCounter = 6;
                 }
                 else
                     continue;
@@ -66,8 +83,19 @@ public class CustomerController : MonoBehaviour
             else
             {
                 //update entity like normal
-                customerEntities[i].UpdateCustomer();
+                //check if customer reach the end position
+                if (! customerEntities[i].UpdateCustomer())
+                {
+                    //can remove customer
+                    Destroy(customerEntities[i].gameObject);
+                    customerEntities[i] = null;
+                }
             }
         }
+    }
+
+    public void CloseStore()
+    {
+        storeClosed = true;
     }
 }
