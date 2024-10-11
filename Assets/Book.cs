9de750001838463d383a33264135028a8b498e1d
KapitanKohl <eldrick.andrew05@gmail.com>
2024-10-11 19:02:59 +0800

    black

---
+++ Assets/Book.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Book", menuName = "Scriptable Object/Book")]
+public class Book : ScriptableObject
+{
+    public Sprite bookSprite;
+}
