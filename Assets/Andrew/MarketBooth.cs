296371a8507d63e1594fd20f0237591c7e726912
Zehui2020 <zehui2020@gmail.com>
2024-10-11 21:39:15 +0800

    Fixes

--- Assets/Andrew/MarketBooth.cs
+++ Assets/Andrew/MarketBooth.cs
@@ -1,12 +1,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class MarketBooth : MonoBehaviour
 {
     [SerializeField] private Animator boothAnimator;
     [SerializeField] protected ConfirmBuyPanel confirmBuyPanel;
     [SerializeField] protected RectTransform buttonParent;
+
+    [SerializeField] private Sprite shopkeeperSprite;
+    [SerializeField] private Image shopIcon;
 
     public UnityEvent OnExitShop;
 
@@ -24,6 +28,7 @@ public class MarketBooth : MonoBehaviour
 
     public void EnterShop()
     {
+        shopIcon.sprite = shopkeeperSprite;
         InitBooth();
         boothAnimator.SetBool("isShopping", true);
         PlayerStats.playerStatsInstance.playerMarketState = PlayerStats.PlayerMarketState.InMenu;
@@ -31,6 +36,7 @@ public class MarketBooth : MonoBehaviour
 
     public void OpenPannel()
     {
+        shopIcon.sprite = shopkeeperSprite;
         boothAnimator.SetBool("isOpenPannel", true);
     }
 
